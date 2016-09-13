56cd91b0b65b8c15fc53b9d26c74f859743ecd57
tfogg2 <foggts@g.cofc.edu>
2016-09-13 13:16:55 -0400

    filter-fly 7

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,7 @@
 ShopifyApp.configure do |config|
   config.api_key = "fa263eb3af1d0c9fa0b9bfd797a3a613"
   config.secret = "0ca0c3ddffe2b527ec59c49b47234e1b"
-  config.redirect_uri = "https://filter-fly.herokuapp.com/auth/shopify/callback"
+ 
   config.scope = "read_orders, read_products"
   config.embedded_app = true
 end
