8fe41917dfa4fa926538ba53daf9b5371dea2e64
tfogg2 <foggts@g.cofc.edu>
2016-09-13 12:35:55 -0400

    filter-fly 1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FilterFly::Application.config.secret_key_base = '540704269ae56423b67138057db400e2a5b17e62842ee83420252dce192870bc2afdfedb656d37272e0f14b599232b1cf764bafc604c5483ffc20779a91640d2'
