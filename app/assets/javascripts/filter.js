3c552faed0a1b52123b721685a98d9f327fe94d2
tfogg2 <foggts@g.cofc.edu>
2016-09-13 23:32:49 -0400

    filter-fly logic1

---
+++ app/assets/javascripts/filter.js
@@ -0,0 +1,15 @@
+var $filter = document.getElementById('filter');
+	$filter.append($newFilter)
+	$filter.append($tagSearch)
+
+var $tagSearch = $('<input class="tagSearch", type="submit", value="submit"><input>')
+	$tagSearch.hide()
+
+var $newFilter = $('<button class="newFilter"></button>');
+	if ($newFilter.click(function(){
+		$newFilter.append($tagSearch);
+		$tagSearch.show();
+		
+
+
+	}));
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ FilterFly::Application.routes.draw do
   end
 
   root :to => 'home#index'
+  get 'filter' => 'main#filter' 
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
