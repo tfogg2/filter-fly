93cba56b257d11c221e1c7314544662ad6633ef0
tfogg2 <foggts@g.cofc.edu>
2016-10-02 15:28:24 -0400

    webhooks

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,14 +1,11 @@
 ShopifyApp.configure do |config|
   config.api_key = "fa263eb3af1d0c9fa0b9bfd797a3a613"
   config.secret = "0ca0c3ddffe2b527ec59c49b47234e1b"
- 
-  config.scope = "read_orders, read_products"
+  config.scope = "read_products, write_products"
   config.embedded_app = true
   config.webhooks = [
   	{topic: 'collections/create', address: 'http://filter-fly.herokuapp.com/webhooks/collections_create', format: 'json'}
-=begin
   	{topic: 'collections/delete', address: 'http://filter-fly.herokuapp.com/webhooks/collections_delete', format: 'json'}
   	{topic: 'collections/update', address: 'http://filter-fly.herokuapp.com/webhooks/collections_update', format: 'json'}
-=end 
   ]
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,14 @@ Filterfly::Application.routes.draw do
 
   
 
+
   controller :sessions do
     get 'login' => :new, :as => :login
     post 'login' => :create, :as => :authenticate
     get 'auth/shopify/callback' => :callback
     get 'logout' => :destroy, :as => :logout
   end
-  root 'home#index'
+
   get 'design' => 'refinements#design', as: :design
   get 'installation' => 'support#installation', as: :installation
   get 'support' => 'support#support', as: :support
