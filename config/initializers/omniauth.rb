373012525cbc8cf196049a6e668a82adef40b3d1
tfogg2 <foggts@g.cofc.edu>
2016-10-03 17:05:01 -0400

    FIX attempt

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,8 +3,9 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     ShopifyApp.configuration.api_key,
     ShopifyApp.configuration.secret,
     :scope => ShopifyApp.configuration.scope,
+=begin
     :redirect_uri => "https://filter-fly.herokuapp.com/auth/shopify/callback",
-
+=end
     :setup => lambda {|env|
        params = Rack::Utils.parse_query(env['QUERY_STRING'])
        site_url = "https://#{params['shop']}"
