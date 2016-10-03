79a008de653c3054e995182dd27b845ab747381f
tfogg2 <foggts@g.cofc.edu>
2016-10-03 17:00:01 -0400

    added logo

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,14 +2,14 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :shopify,
     ShopifyApp.configuration.api_key,
     ShopifyApp.configuration.secret,
-    redirect_uri: ShopifyApp.configuration.redirect_uri,
     :scope => ShopifyApp.configuration.scope,
+    :redirect_uri => "https://filter-fly.herokuapp.com/auth/shopify/callback",
 
     :setup => lambda {|env|
        params = Rack::Utils.parse_query(env['QUERY_STRING'])
        site_url = "https://#{params['shop']}"
        env['omniauth.strategy'].options[:client_options][:site] = site_url
-       
+
     }
 
 end
