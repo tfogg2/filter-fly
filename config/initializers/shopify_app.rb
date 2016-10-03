835b8c50574f6be5794ece42ee1b5728069feac3
tfogg2 <foggts@g.cofc.edu>
2016-10-03 17:22:08 -0400

    fixed heroku but deleted webhooks 2

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,11 +1,9 @@
 ShopifyApp.configure do |config|
   config.api_key = "fa263eb3af1d0c9fa0b9bfd797a3a613"
   config.secret = "0ca0c3ddffe2b527ec59c49b47234e1b"
-  
+  config.scope = "read_products, write_products"
   config.embedded_app = true
 =begin
-config.scope = "read_products, write_products"
-
   config.webhooks = [
   		{topic: 'collections/create', address: 'http://filter-fly.herokuapp.com/webhooks/collections_create', format: 'json'}
   	]
