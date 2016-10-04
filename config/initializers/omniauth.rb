9687c2a4f93d96c428614a6982740f49e69c3972
Mike Fogg <me@mikefogg.co>
2016-10-04 12:15:30 -0400

    Updating shopify and rails version

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,14 +2,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :shopify,
     ShopifyApp.configuration.api_key,
     ShopifyApp.configuration.secret,
-    :scope => ShopifyApp.configuration.scope,
-    :redirect_uri => ShopifyApp.configuration.redirect_uri,
-
-    :setup => lambda {|env|
-       params = Rack::Utils.parse_query(env['QUERY_STRING'])
-       site_url = "http://#{params['shop']}"
-       env['omniauth.strategy'].options[:client_options][:site] = site_url
-
-    }
-
+    scope: ShopifyApp.configuration.scope
 end
--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -2,15 +2,10 @@ ShopifyApp.configure do |config|
   config.api_key = "fa263eb3af1d0c9fa0b9bfd797a3a613"
   config.secret = "0ca0c3ddffe2b527ec59c49b47234e1b"
   config.scope = "read_products, write_products"
-  config.redirect_uri = "http://localhoast:3000/auth/shopify/callback"
   config.embedded_app = true
-=begin
   config.webhooks = [
-  		{topic: 'collections/create', address: 'http://filter-fly.herokuapp.com/webhooks/collections_create', format: 'json'}
-
-  		{topic: 'collections/delete', address: 'http://filter-fly.herokuapp.com/webhooks/collections_delete', format: 'json'}
-  		{topic: 'collections/update', address: 'http://filter-fly.herokuapp.com/webhooks/collections_update', format: 'json'}
-
-  	]
-=end
+		{topic: 'collections/create', address: 'http://filter-fly.herokuapp.com/webhooks/collections_create', format: 'json'},
+		{topic: 'collections/delete', address: 'http://filter-fly.herokuapp.com/webhooks/collections_delete', format: 'json'},
+		{topic: 'collections/update', address: 'http://filter-fly.herokuapp.com/webhooks/collections_update', format: 'json'}
+  ]
 end
